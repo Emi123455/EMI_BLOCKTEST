3d7e5bdcc5991fb2571bc6bd3fb71e80649bdcf8
Emi123455 <120152851+Emi123455@users.noreply.github.com>
2022-12-09 10:46:29 +0545

    Update main.java

--- src/model/main.java
+++ src/model/main.java
@@ -14,19 +14,18 @@ public class main {
         Items i4=new Items("I4","1004","b8",1400);
         Items i5=new Items("I5","1005","b9",1500);
 
-        //creating an arraylist to store the items ordered by the user.
+      
         ArrayList<Items> items_u1=new ArrayList<>();
 
         items_u1.add(i1);
         items_u1.add(i2);
 
-        //creating an arraylist to store the items ordered by the second user.
+      
 
         ArrayList<Items> items_u2=new ArrayList<>();
         items_u2.add(i3);
         items_u2.add(i4);
 
-        //creating an arraylist to store the number of orders of an user.
         ArrayList<Orders> orders_u1=new ArrayList<>();
         Orders u1_o1=new Orders("u1",items_u1);
         Orders u1_o2=new Orders("u1",items_u1);
@@ -34,7 +33,7 @@ public class main {
         orders_u1.add(u1_o1);
         orders_u1.add(u1_o2);
 
-        //creating an arraylist to store the number of orders of second user.
+        
         ArrayList<Orders> orders_u2=new ArrayList<>();
         Orders u2_o1=new Orders("u2",items_u2);
         Orders u2_o2=new Orders("u2",items_u2);
